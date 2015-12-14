6f910696d2df9c5f29e8ec3f1dcb49f7a3e8843b
Loek van der Linde <lind0077@hz.nl>
2015-12-14 13:59:10 +0100

    SQL complete!

--- triggertest.sql
+++ triggertest.sql
@@ -14,8 +14,8 @@
 
 # We'll start with an easy table to add some data to
 CREATE TABLE ship (
-  ship_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
+  ship_id INT(8) AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
 ) ENGINE = InnoDB;
 
 # Now the audit table
@@ -24,10 +24,10 @@ CREATE TABLE ship (
 # SET @username := 'admin'; and add a user row in this table
 # NOTE operation is 'INS' - 'UPD' - 'DEL'
 CREATE TABLE ship_audit (
-  ship_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  ship_id INT(8) AUTO_INCREMENT PRIMARY KEY,
   operation CHAR(3),
   updatedate DATETIME,
-  name VARCHAR(255),
+  name VARCHAR(255)
 ) ENGINE = InnoDB;
 
 /******************************************************************************
@@ -35,7 +35,7 @@ CREATE TABLE ship_audit (
 ******************************************************************************/
 
 # Define a delimiter for the entire following block now
-DELIMITER $$
+delimiter //
 
 # Start with the insert trigger. This gets called and run whenever an insert
 # operation is run on the ship table. It adds a row into the audit table with
@@ -44,32 +44,33 @@ CREATE DEFINER = CURRENT_USER TRIGGER ship_audit_ins
 AFTER INSERT
 ON ship
 FOR EACH ROW
+BEGIN
   INSERT INTO ship_audit (ship_id, operation, updatedate, name)
   VALUES (NEW.SHIP_ID, 'INS', NOW(), NEW.NAME);
 END;
-$$
+//
 
 # And the update trigger. This gets called and run whenever an upate
 # operation is run on the ship table. It adds a row into the audit table with
 # the changes occured into the original ship table
-CREATE DEFINER = CURRENT_USER TRIGGER ship_audit_ins
+CREATE DEFINER = CURRENT_USER TRIGGER ship_audit_upd
 AFTER UPDATE
 ON ship
 FOR EACH ROW
+BEGIN
   INSERT INTO ship_audit (ship_id, operation, updatedate, name)
   VALUES (NEW.SHIP_ID, 'UPD', NOW(), NEW.NAME);
 END;
-$$
+//
 
 # Now the delete trigger. This gets called and run whenever a delete
 # operation is run on the ship table. It adds a row into the audit table with
 # the changes occured into the original ship table
-CREATE DEFINER=CURRENT_USER TRIGGER ship_audit_del
+CREATE DEFINER = CURRENT_USER TRIGGER ship_audit_del
 AFTER DELETE
 ON ship
 FOR EACH ROW
 BEGIN
-  INSERT INTO ship_audit (ship_id, operation, updatetime, name)
+  INSERT INTO ship_audit (ship_id, operation, updatedate, name)
   VALUES (OLD.SHIP_ID, 'DEL', NOW(), OLD.NAME);
 END;
-$$
