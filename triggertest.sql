6e28552bb51dfbd28c7a3fe6a0f8d0578527717f
Loek van der Linde <lind0077@hz.nl>
2015-12-14 12:48:46 +0100

    Start with SQL file

---
+++ triggertest.sql
@@ -0,0 +1,24 @@
+-- This is a proof of concept for auditing data in MySQL databases.
+-- It works by adding triggers to tables you want to audit. The triggers
+-- make sure the changed/inserted/deleted data gets written to another
+-- table, which is the Audit table. By reading out the Audit table,
+-- you can see what happened with all the data in the database
+
+-- SET UP DATABASE
+
+-- We'll start with an easy table to add some data to
+
+CREATE TABLE ship (
+  ship_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+) ENGINE = InnoDB;
+
+-- Now the audit table
+
+CREATE TABLE ship_audit (
+  ship_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  operation CHAR(1),
+  updatedate DATETIME,
+  name DECIMAL(10,2),
+  user VARCHAR(30)
+) ENGINE = InnoDB;
