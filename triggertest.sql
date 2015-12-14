3d8deb9eb8521590c5b2fd83c844cf147d3f433b
Loek van der Linde <lind0077@hz.nl>
2015-12-14 12:56:59 +0100

    Better comments

--- triggertest.sql
+++ triggertest.sql
@@ -1,24 +1,31 @@
--- This is a proof of concept for auditing data in MySQL databases.
--- It works by adding triggers to tables you want to audit. The triggers
--- make sure the changed/inserted/deleted data gets written to another
--- table, which is the Audit table. By reading out the Audit table,
--- you can see what happened with all the data in the database
+/******************************************************************************
+*
+* This is a proof of concept for auditing data in MySQL databases.
+* It works by adding triggers to tables you want to audit. The triggers
+* make sure the changed/inserted/deleted data gets written to another
+* table, which is the Audit table. By reading out the Audit table,
+* you can see what happened with all the data in the database
+*
+******************************************************************************/
 
--- SET UP DATABASE
+/******************************************************************************
+*                               SET UP DATABASE
+******************************************************************************/
 
--- We'll start with an easy table to add some data to
-
+# We'll start with an easy table to add some data to
 CREATE TABLE ship (
   ship_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
 ) ENGINE = InnoDB;
 
--- Now the audit table
-
+# Now the audit table
+# NOTE to use this user row (given no user is specified in the triggers)
+# you have to specify it somewhere in the DB connection with:
+# SET @username := 'admin';
 CREATE TABLE ship_audit (
   ship_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   operation CHAR(1),
   updatedate DATETIME,
-  name DECIMAL(10,2),
-  user VARCHAR(30)
+  name DECIMAL(10,2)
+/*  user VARCHAR(30)  */
 ) ENGINE = InnoDB;
